338ba7e6aa307ad33f0eb82d17b1a031489527d3
qiushye <qiushye@163.com>
2018-10-26 22:23:54 +0800

    Add files via upload

---
+++ compt_methods.py
@@ -0,0 +1,322 @@
+#encoding=utf-8
+import random
+import scipy
+import sktensor
+import time
+import sys
+import os
+#import scipy.io as scio
+import numpy as np
+#from random import random
+from sktensor import tucker,cp
+from scipy.sparse import rand as sprand
+from sktensor.dtensor import dtensor, unfolded_dtensor
+from sktensor.sptensor import unfolded_sptensor
+
+from sklearn.cluster import KMeans
+#from test_methods import deal_orimiss
+import tensor_cpt as tc
+import scipy.io as scio
+os.sys.path.append('./ppca-master/src/')
+import pca
+from sklearn.model_selection import train_test_split
+
+#Truncated SVD
+def T_SVD(Atensor,p):
+    SD = dtensor(Atensor.copy())
+    N = len(np.shape(SD))
+    U_list,r_list = [],[]
+    SG = []
+    for i in range(N):
+        B = SD.unfold(i)
+        U,sigma,VT = scipy.linalg.svd(B,0)
+        row_s = len(sigma)
+        mat_sig = np.zeros((row_s,row_s))
+        for j in range(row_s):
+            mat_sig[j,j] = sigma[j]
+            if sum(sigma[:j])/sum(sigma) > p:
+                SG.append(sigma[j])
+                break
+
+        U_list.append(U[:,:j])
+        r_list.append(j)
+    return SG,j,U_list,r_list
+
+def traffic_info(data):
+    #data, miss_data1, W1, ori_W1 = handle_info
+    #data = miss_data1  # change
+    ds = data.shape
+    var_mat = np.zeros((ds[0], ds[1]))
+    mean_mat = np.zeros((ds[0], ds[1]))
+    if len(ds) == 3:
+        for r in range(ds[0]):
+            for d in range(ds[1]):
+                var_mat[r, d] = np.var(data[r, d, :])
+                mean_mat[r, d] = np.mean(data[r, d, :])
+    else:
+        for r in range(ds[0]):
+            std = np.std(data[r])
+            mean = np.mean(data[r])
+            for d in range(ds[1]):
+                var_mat[r, d] = (data[r, d] - mean) / std
+                mean_mat[r, d] = data[r, d]
+    return var_mat,mean_mat
+
+#tucker�ֽ����䷽��
+def tucker_cpt(sparse_data,rank_list,W):
+    time_s = time.time()
+    est_data = sparse_data.copy()
+    dshape = np.shape(est_data)
+    SD = dtensor(est_data)
+    #U = tucker.hosvd(SD,rank_list)
+    core1,U1 = tucker.hooi(SD,rank_list,init='nvecs')
+    #print('mean,var',np.mean(core1.unfold(0)),core1.var)
+    #print('U_mean',(U1[0]==0).max())
+    left1 = SD.unfold(0)
+    U_1,sigma,VT = np.linalg.svd(left1,0)
+    #print(np.sum(U1[0]-U_1))
+    #ttm:����˷�
+    ttm_data = core1.ttm(U1[0],0).ttm(U1[1],1).ttm(U1[2],2)
+    print(np.linalg.norm(ttm_data-sparse_data))
+    #print(np.mean(ttm_data))
+    est_data = W*est_data+(W==False)*ttm_data
+    time_e = time.time()
+    print('-'*8+'tucker'+'-'*8)
+    print('exec_time:'+str(time_e-time_s)+'s')
+    return est_data
+
+def multi_tucker(sparse_data,rates,W):
+    SD = dtensor(sparse_data)
+    est_dict = {}
+    for rate in rates:
+        rank_set = [0,0,0]
+        for i in range(3):
+            U,sigma,VT = scipy.linalg.svd(SD.unfold(i),0)
+            for r in range(len(sigma)):
+                if sum(sigma[:r])/sum(sigma) > rate:
+                    rank_set[i] = r
+                    break
+        print(rank_set)
+        est_dict[rate] = tucker_cpt(sparse_data,rank_set,W)
+    return est_dict
+
+#STD���
+def STD_cpt(sparse_data,W,threshold=1e-4, alpha=2e-10, lm=0.01, p=0.7):
+    ds = sparse_data.shape
+    X_ori = sparse_data.copy()
+    U_list,r_list = T_SVD(X_ori,p)[-2:]
+    core, U_list = tucker.hooi(dtensor(X_ori), r_list, init='nvecs')
+    [A,B,C] = U_list
+    #core = dtensor(X_ori).ttm(A.T, 0).ttm(B.T, 1).ttm(C.T, 2)
+    X = core.ttm(A, 0).ttm(B, 1).ttm(C, 2)
+    #print(np.linalg.norm(X-X_ori))
+    #return
+    Upre_list = U_list
+    F_diff = sys.maxsize
+    iter = 0
+    while F_diff > threshold and iter < 500:
+        X_pre = X.copy()
+        #print('Xpre_norm',np.linalg.norm(X_pre))
+        # Upre_list = []
+        # for u in U_list:
+        #     Upre_list.append(u.copy())
+        core_pre = core.copy()
+        E = W*(X_ori-core_pre.ttm(Upre_list[0], 0).ttm(Upre_list[1], 1).ttm(Upre_list[2], 2))
+        for i in range(X.ndim):
+            mul1 = (W*E).unfold(i)
+            if i == 0:
+                mul2 = np.kron(Upre_list[2],Upre_list[1])
+            elif i == 1:
+                mul2 = np.kron(Upre_list[2],Upre_list[0])
+            else:
+                mul2 = np.kron(Upre_list[1],Upre_list[0])
+            mul3 = core_pre.unfold(i).T
+            Upre_list[i] = (1-alpha*lm)*Upre_list[i]+alpha*np.dot(np.dot(mul1,mul2),mul3)
+            #print(np.dot(mul1,mul2))
+        Temp = E.ttm(Upre_list[0].T, 0).ttm(Upre_list[1].T, 1).ttm(Upre_list[2].T, 2)
+        core = (1-alpha*lm)*core_pre+alpha*Temp
+        X = core.ttm(Upre_list[0],0).ttm(Upre_list[1],1).ttm(Upre_list[2],2)
+        F_diff = np.linalg.norm(X-X_pre)
+       #break
+        iter += 1
+    return X
+
+#CP�ֽ����䷽��
+def cp_cpt(sparse_data,rank,W):
+    time_s = time.time()
+    est_data = sparse_data.copy()
+    dshape = np.shape(est_data)
+    SD = dtensor(sparse_data.copy())
+    U = []
+    P,fit,itr,arr = cp.als(SD,rank)
+    loc_data = P.totensor()
+    est_data = W*est_data+(W==False)*loc_data
+    time_e = time.time()
+    print('-'*8+'cp'+'-'*8)
+    print('exec_time:'+str(time_e-time_s)+'s')
+    return est_data
+
+#LRTC����䷽��
+def lrtc_cpt(sparse_data,alpha,beta,gama,conv_thre,K,W):
+    time_s = time.time()
+    Y = sparse_data.copy()
+    N = len(np.shape(sparse_data))
+    X = Y.copy()
+    normY = np.sum(Y**2)**0.5
+    M = {}
+    MX,MY,M_fold = {},{},{}
+    for iter in range(K):
+        Y_pre = Y.copy()
+        for n in range(N):
+            
+            MX[n] = dtensor(X).unfold(n)
+            MY[n] = dtensor(Y).unfold(n)
+            M_temp = (alpha[n]*MX[n]+beta[n]*MY[n])/(alpha[n]+beta[n])
+            para_fi = gama[n]/(alpha[n]+beta[n])
+            U,sigma,VT = np.linalg.svd(M_temp,full_matrices=0)
+            row_s = len(sigma)
+            mat_sig = np.zeros((row_s,row_s))
+            max_rank = 0
+            for ii in range(row_s):
+                mat_sig[ii,ii] = max(sigma[ii]-para_fi,0)
+            M[n] = np.dot(np.dot(U[:,:row_s],mat_sig),VT[:row_s,:])
+            M_fold[n] = M[n].fold()
+        X = np.sum([alpha[i]*M_fold[i] for i in range(N)],axis=0)/sum(alpha)
+        Y_temp = np.sum([beta[i]*M_fold[i] for i in range(N)],axis=0)/sum(beta)
+        Y[W==False] = Y_temp[W==False]
+        Y_Fnorm = np.sum((Y-Y_pre)**2)
+        if Y_Fnorm < conv_thre:
+            break
+    time_e = time.time()
+    print('-'*8+'lrtc'+'-'*8)
+    print('exec_time:'+str(time_e-time_s)+'s')
+    return Y
+ 
+def silrtc_cpt(sparse_data,alpha,beta,conv_thre,K,W):
+    time_s = time.time()
+    X = sparse_data.copy()
+    M = {}
+    N = len(np.shape(X))
+    for iter in range(K):
+        X_pre = X.copy()
+        for i in range(N):
+            para_fi = alpha[i]/beta[i]
+            U,sigma,VT = np.linalg.svd(dtensor(X).unfold(i),full_matrices=0)
+            row_s = len(sigma)
+            mat_sig = np.zeros((row_s,row_s))
+            for ii in range(row_s):
+                mat_sig[ii,ii] = max(sigma[ii]-para_fi,0)
+            M[i] = (np.dot(np.dot(U,mat_sig),VT[:row_s,:])).fold()
+        X_temp = np.sum([beta[j]*M[j] for j in range(N)],axis=0)/sum(beta)
+        X[W==False] = X_temp[W==False]
+        X_diffnorm = np.sum((X-X_pre)**2)
+        if X_diffnorm < conv_thre:
+            break
+    time_e = time.time()
+    print('-'*8+'silrtc'+'-'*8)
+    print('exec_time:'+str(time_e-time_s)+'s')
+    return X
+
+def halrtc_cpt(sparse_data,lou,conv_thre,K,W,alpha=[0,0,1]):
+    #ori_speeddata = scio.loadmat('../GZ_data/speed_tensor.mat')['tensor']
+    #ori_speeddata, ori_W = tc.deal_orimiss(ori_speeddata, False)
+    time_s = time.time()
+    X = sparse_data.copy()
+    Y = {}
+    N = len(np.shape(X))
+    W1 = (W==False)
+    M = {}
+    T_temp = X.copy()
+    #alpha = [1/4,1/4,1/4,1/4] if N==4 else [0,0,1]
+    for _ in range(N):
+        M[_] = dtensor(np.zeros(np.shape(X)))
+        Y[_] = dtensor(np.zeros(np.shape(X)))
+    for iter in range(K):
+        X_pre = X.copy()
+        T_temp_pre = T_temp.copy()
+        for i in range(N):
+            SD = dtensor(X_pre)
+            Matrix = SD.unfold(i)+1/lou*(Y[i].unfold(i))
+
+            U,sigma,VT = np.linalg.svd(Matrix,0)
+            row_s = len(sigma)
+            mat_sig = np.zeros((row_s,row_s))
+            for ii in range(row_s):
+                mat_sig[ii,ii] = max(sigma[ii]-alpha[i]/lou,0)
+            M[i] = (np.dot(np.dot(U,mat_sig),VT[:row_s,:])).fold()
+
+        T_temp = (np.sum([M[j]-1/lou*Y[j] for j in range(N)],axis=0))/N
+        X[W1] = T_temp[W1]
+        X_Fnorm = np.sum((X-X_pre)**2)
+        if X_Fnorm < conv_thre:
+            break
+        for i in range(N):
+            Y[i] -= lou*(M[i]-X)
+    time_e = time.time()
+    #print('-'*8+'halrtc'+'-'*8)
+    #print('exec_time:'+str(time_e-time_s)+'s')
+    print('iter:',iter)
+    return X
+
+def PPCA_cpt(sparse_data,p=0.7):
+    time_s = time.time()
+    ds = sparse_data.shape
+    est_PPCA = np.zeros_like(sparse_data)
+    SD = sparse_data.copy()
+    for i in range(ds[0]):
+        data = SD[i]
+        mult_pca_components = T_SVD(data, p)[-1][0]
+        ppca = pca.ppca.PPCA(q=mult_pca_components)
+        ppca.fit(data)
+        est_PPCA[i] = ppca.transform_infers()
+    time_e = time.time()
+    print('-' * 8 + 'PPCA' + '-' * 8)
+    print('exec_time:' + str(time_e - time_s) + 's')
+    return est_PPCA
+
+def BPCA_cpt(sparse_data,p=0.7):
+    time_s = time.time()
+    ds = sparse_data.shape
+    est_BPCA = np.zeros_like(sparse_data)
+    SD = sparse_data.copy()
+    for i in range(ds[0]):
+        data = SD[i]
+        mult_pca_components = T_SVD(data, p)[-1][0]
+        bppca = pca.bppca.BPPCA(data,q=mult_pca_components)
+        bppca.fit()
+        est_BPCA[i] = bppca.transform_infers()
+    time_e = time.time()
+    print('-' * 8 + 'BPCA' + '-' * 8)
+    print('exec_time:' + str(time_e - time_s) + 's')
+    return est_BPCA
+
+def cluster_ha(labels,sparse_data,W,cluster_num,halrtc_para, alpha):
+    sd = sparse_data.copy()
+    Clr_mat = {i:[] for i in range(cluster_num)}
+    for i in range(len(labels)):
+        Clr_mat[labels[i]].append(i)
+    [lou,K,conv_thre] = halrtc_para
+    WT = W.copy()
+    est_data = np.zeros_like(sd)
+    for j in range(cluster_num):
+        m_data = sd[labels == j]
+        Wm = WT[labels == j]
+        temp_data = halrtc_cpt(m_data,lou,conv_thre,K,Wm,alpha)
+        est_data[Clr_mat[j]] = temp_data
+    return est_data
+
+def Kmeans_ha(sparse_data,W, K_n, K, conv_thre, p, alpha=[0,0,1]):
+    SD = sparse_data.copy()
+    time0 = time.time()
+    lou = 1/T_SVD(SD,p)[0][0]
+    #alpha = [0,0,1]
+    halrtc_para = [lou,K,conv_thre]
+    var_mat, mean_mat = traffic_info(SD)
+    clf = KMeans(n_clusters=K_n)
+    S = clf.fit(var_mat)
+    L = S.labels_
+    est_Kmeans = cluster_ha(L, SD, W, K_n, halrtc_para, alpha)
+    time1 = time.time()
+    print('-' * 8 + 'Kmeans++_ha' + '-' * 8)
+    print('exec_time:' + str(time1 - time0) + 's')
+    return est_Kmeans
---
+++ tensor_cpt.py
@@ -0,0 +1,374 @@
+#encoding=utf-8
+import sktensor
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import sys
+import os
+import scipy.io as scio
+import numpy as np
+np.seterr(divide='ignore', invalid='ignore')
+from random import random
+from scipy.sparse import rand as sprand
+from compt_methods import *
+from road_similar import *
+import seaborn as sns
+
+#根据确定的数据维度大小获取数据，对于缺失部分补平均数
+def get_tensor(mat_file,ori_file,size_=0):
+    ori_data = scio.loadmat(mat_file)['Speed']
+    if size_:
+        p,q,r = size_
+        ori_data = ori_data[:p,:q,:r]
+    scio.savemat(ori_file,{'Speed':ori_data})
+    return ori_data
+
+#根据缺失率和原始数据进行随机缺失并保存缺少数据
+def gene_rand_sparse(ori_data, miss_ratio,miss_file):
+    data = ori_data.copy()
+    dshape = np.shape(data)
+    rand_ts = np.random.random_sample(dshape)
+    zero_ts = np.zeros(dshape)
+    data = data*(rand_ts>miss_ratio) + zero_ts*(rand_ts<=miss_ratio)
+    #if not os.path.exists(miss_file):
+    scio.savemat(miss_file,{'Speed':data})
+    return data 
+
+#根据缺失率和原始数据进行连续缺失并保存缺失数据
+def gene_cont_sparse(ori_data,miss_ratio,miss_file):
+    data = ori_data.copy()
+    dshape = data.shape
+    rand_ts = np.random.random_sample(dshape[:-1])
+    S = np.rint(rand_ts+0.5-miss_ratio)
+    W_cont = np.zeros(dshape)
+    for k in range(dshape[2]):
+        W_cont[:,:,k] = S[:,:]
+    data = data*W_cont
+    scio.savemat(miss_file,{'Speed':data})
+    return data
+
+gene_sparse = {'rand':gene_rand_sparse,'cont':gene_cont_sparse}
+
+#获取缺失数据，缺失位置和真实缺失率
+def get_sparsedata(miss_file):
+    #miss_loc = []
+    sparse_data = scio.loadmat(miss_file)['Speed']
+    dshape = np.shape(sparse_data)
+    W_miss = sparse_data==0
+    true_miss_ratio = W_miss.sum()/sparse_data.size
+    return sparse_data,W_miss,true_miss_ratio
+
+#处理原始的缺失数据
+def deal_orimiss(ori_data,shorten = False):
+    p,q,r = ori_data.shape
+    zero_mat = np.zeros((p,q,r))
+    sparse_data = (ori_data>1)*ori_data+(ori_data<=1)*zero_mat
+    W = sparse_data>0
+    W_miss = np.where(sparse_data<=1)
+    M_pos = np.where(W_miss)
+    if not shorten:
+        for i in range(len(W_miss[0])):
+            pos1,pos2,pos3 = W_miss[0][i],W_miss[1][i],W_miss[2][i]
+            neigh_info = []
+            for n2 in (1,-1):
+                for n3 in (1,-1):
+                    try:
+                        temp = sparse_data[pos1,pos2+n2,pos3+n3]
+                        neigh_info.append(temp)
+                    except:
+                        pass
+            if sum(neigh_info)>0:
+                sparse_data[pos1,pos2,pos3] = sum(neigh_info)/(np.array(neigh_info)>0).sum()
+        return sparse_data,W
+            
+    Arr = [set(arr.tolist()) for arr in M_pos]
+    Arr_len = [len(arr) for arr in Arr]
+    Arr_short = Arr_len.index(min(Arr_len))
+    sparse_data = np.delete(sparse_data,list(Arr[Arr_short]),Arr_short)
+    return sparse_data,W
+
+#用奇异值比例来确定秩
+def tk_rank(data,thre = 0.9):
+    rank_set = [0,0,0]
+    for i in range(3):
+        if np.isnan(data).all():
+            print(np.isnan(data).sum())
+            sys.exit()
+        if np.isnan(data).all():
+            print(np.isinf(data).sum())
+            sys.exit()
+        U,sigma,VT = scipy.linalg.svd(dtensor(data).unfold(i),0)
+        for r in range(len(sigma)):
+            if sum(sigma[:r])/sum(sigma) > thre:
+                rank_set[i] = r
+                break
+    return rank_set
+
+#预填充，这里采用同一线圈同一时段不同日期的平均数填充
+def pre_impute(sparse_data,W,day_axis=1,bias_bool = False):
+    if not bias_bool:
+        pos = np.where(W==False)
+        for p in range(len(pos[0])):
+            i,j,k = pos[0][p],pos[1][p],pos[2][p]
+            if day_axis == 0:
+                if (sparse_data[:,j,k]>0).sum()>0:
+                    sparse_data[i,j,k] = np.sum(sparse_data[:,j,k])/(sparse_data[:,j,k]>0).sum()
+                else:
+                    sparse_data[i,j,k] = np.sum(sparse_data[i,:,:])/(sparse_data[i,:,:]>0).sum()
+            elif day_axis == 1:
+                if (sparse_data[i,:,k]>0).sum()>0:
+                    sparse_data[i,j,k] = np.sum(sparse_data[i,:,k])/(sparse_data[i,:,k]>0).sum()
+                else:
+                    sparse_data[i,j,k] = np.sum(sparse_data[:,j,:])/(sparse_data[:,j,:]>0).sum()
+        return sparse_data
+    b,b1 = {},{}
+    sp = np.shape(sparse_data)
+    impute_data = sparse_data.copy()
+    mean = np.sum(impute_data)/(impute_data>0).sum()
+    for n in range(3):
+        b[n] = np.random.uniform(0,0.1,sp[n])
+        b1[n] = np.zeros(sp[n])
+    seta,miu = 1e-4,800
+    sum_F = sum([np.sum(b[n]**2) for n in range(3)])
+    J = 1/2*np.sum(W*(sparse_data-impute_data)**2)+miu/2*sum_F
+    ite = 0
+    while ite < 100:
+        ite += 1
+        J_pre = J
+        for i in range(sp[0]):
+            b1[0][i] = (1-seta*miu)*b[0][i]+seta*np.sum(W[i,:,:]*(sparse_data-impute_data)[i,:,:])
+        for j in range(sp[1]):
+            b1[1][j] = (1-seta*miu)*b[1][j]+seta*np.sum(W[:,j,:]*(sparse_data-impute_data)[:,j,:])
+        for k in range(sp[2]):
+            b1[2][k] = (1-seta*miu)*b[2][k]+seta*np.sum(W[:,:,k]*(sparse_data-impute_data)[:,:,k])
+        if sum([np.sum((b1[n]-b[n])**2)**0.5 for n in range(3)]) < 0.001:
+            pass
+        for n in range(3): 
+            b[n] = b1[n].copy()
+        for i in range(sp[0]):
+            for j in range(sp[1]):
+                for k in range(sp[2]):
+                    impute_data[i,j,k] = mean+b[0][i]+b[1][j]+b[2][k]
+        sum_F = sum([np.sum(b[n]**2) for n in range(3)])
+        J = 1/2*np.sum(W*(sparse_data-impute_data)**2)+miu/2*sum_F
+        if abs(J-J_pre) < 1:
+            break
+    print(np.sum(W*(sparse_data-impute_data)**2))
+    sparse_data[W==False] = mean+b[0][i]+b[1][j]+b[2][k]
+    return sparse_data
+
+#求rmse,mape,mae,rse
+def rmse_mape_rse(est_data,ori_data,W):
+    S = np.shape(est_data)
+    W_miss = (W==False)
+    diff_data = np.zeros(S)+W_miss*(est_data-ori_data)
+    #diff_data = ori_data-est_data
+    rmse = float((np.sum(diff_data**2)/W_miss.sum())**0.5)
+    mre_mat=np.zeros_like(est_data)
+    mre_mat[W_miss]=np.abs((est_data[W_miss]-ori_data[W_miss])/ori_data[W_miss])
+    mape = float(np.sum(mre_mat)/W_miss.sum())*100
+    rse = float(np.sum(diff_data**2)**0.5/np.sum(ori_data[W_miss]**2)**0.5)
+    mae = float(np.sum(np.abs(diff_data))/W_miss.sum())
+    return round(rmse,4),round(mape,2),round(rse,4),round(mae,4)
+
+def show_img(X,RMSE_list,MAE_list,name_list):
+    for i in range(len(name_list)):
+        plt.plot(X,RMSE_list[i],'--o')
+        plt.savefig(img_dir+'rmse_'+name_list[i])
+        plt.close()
+        plt.plot(X,MAE_list[i],'--o')
+        plt.savefig(img_dir+'mae_'+name_list[i])
+        plt.close()
+    return 0
+
+
+def svd_vary(sparse_data):
+    ds = sparse_data.shape
+    SG = np.zeros((max(ds),3))
+    plt.xlabel('Missing Rate (%)')
+    for i in range(len(ds)):
+        A = dtensor(sparse_data).unfold(i)
+        # U,sigma,VT = np.linalg.svd(A,0)
+        # SG[:len(sigma),i] = sigma
+        for j in range(ds[i]):
+            SG[j,i] = np.var(A[j])
+        plt.plot(list(range(max(ds))),SG[:,i],'r--o',label='$'+str(i)+'$')
+
+
+    plt.legend(loc=1)
+    plt.savefig(img_dir+'svd_vary.png')
+    plt.close()
+    return
+
+#比较三维张量和四维张量的填充结果
+def compare_3d_4d(ori_speeddata,miss_data):
+    time1 = time.time()
+    lou = 1 / T_SVD(miss_data, 0.7)[0][0]
+    #lou = 0.1
+    print(lou)
+    est_halrtc = halrtc_cpt(miss_data, lou, 1e-4, 100, W)
+    time2 = time.time()
+    print('3d_halrtc:', rmse_mape_rse(est_halrtc, ori_speeddata, (W | (ori_W == False))))
+    print('3d_time', str(time2 - time1) + 's')
+    return
+
+def compare_PI(ori_speeddata,ori_W):
+    RM_dict, MA_dict, RS_dict, MP_dict = {}, {}, {}, {}
+    rt_dict = {}
+    miss_list = []
+    # 参数
+    p = 0.7
+    K = 100  # iterations
+    F_thre = 1e-4  # F_norm convergence threshold
+    for i in range(8):
+        print('----'+str(i)+'----')
+        miss_ratio = round(0.1*(i+1),2)
+        miss_path = data_dir+'miss_'+str(miss_ratio)+''.join(['_'+str(ch) for ch in data_size])+'.mat'
+        if not os.path.exists(miss_path):
+            gene_rand_sparse(ori_speeddata,miss_ratio,miss_path)
+        miss_data,W_miss,tm_ratio = get_sparsedata(miss_path)
+        W = (W_miss==False)
+        miss_list.append(round(tm_ratio * 100,1))
+        #预填充
+        #miss_data = pre_impute(miss_data,W,False)
+        rW = W|(ori_W==False)
+        time_s = time.time()
+        K_n = 4  # cluster_num
+        #est_kmeans = Kmeans_ha(miss_data, W, K_n, K, F_thre, p)
+        est_halrtc = halrtc_cpt(miss_data, 1e-3, 1e-4, 100, W,[1/3,1/3,1/3])
+        time_e = time.time()
+        rm, mp, rs, ma = rmse_mape_rse(est_halrtc, ori_speeddata, rW)
+        km = 'Without-PI'
+        if km not in RM_dict:
+            RM_dict[km], MP_dict[km], RS_dict[km], MA_dict[km] = [], [], [], []
+            rt_dict[km] = []
+        RM_dict[km].append(rm)
+        MA_dict[km].append(ma)
+        MP_dict[km].append(mp)
+        RS_dict[km].append(rs)
+        rt_dict[km].append(round(time_e - time_s,1))
+
+        miss_data = pre_impute(miss_data, W, False)
+        rW = W | (ori_W == False)
+        time_s = time.time()
+        K_n = 4  # cluster_num
+        #est_kmeans = Kmeans_ha(miss_data, W, K_n, K, F_thre, p)
+        est_halrtc = halrtc_cpt(miss_data, 1e-3, 1e-4, 100, W,[1/3,1/3,1/3])
+        time_e = time.time()
+        rm, mp, rs, ma = rmse_mape_rse(est_halrtc, ori_speeddata, rW)
+        km = 'With-PI'
+        if km not in RM_dict:
+            RM_dict[km], MP_dict[km], RS_dict[km], MA_dict[km] = [], [], [], []
+            rt_dict[km] = []
+        RM_dict[km].append(rm)
+        MA_dict[km].append(ma)
+        MP_dict[km].append(mp)
+        RS_dict[km].append(rs)
+        rt_dict[km].append(round(time_e - time_s,1))
+
+    eva_dict = {'RMSE': RM_dict, 'MAE': MA_dict, 'MRE': MP_dict, 'Run_Time': rt_dict}
+    metric_dict = {'RMSE': 'km/h', 'MAE': 'km/h', 'MRE': '%', 'Run_Time': 's'}
+    eva_Ylim = {'RMSE': [2, 10], 'MAE': [0, 5], 'MRE': [5, 20], 'Run_Time': [0, 5000]}
+    shape = ['r--o', 'r--*', 'r--x', 'r--^', 'r--s', 'r--D']
+    MK = ['o', 'o', '*', '*', 'x', 'x']
+    CR = ['r', 'b', 'y', 'r', 'b', 'y']
+    fw = open('compare_PI_' + '.txt', 'w')
+    fw.write('Missing Rate (%):' + ','.join(list(map(str, miss_list))) + '\n')
+    for eva in eva_dict:
+        # fig = plt.figure()
+        # ax = fig.add_subplot(1,1,1)
+
+        plt.xlabel('Missing Rate (%)')
+        plt.ylabel(eva + ' (' + metric_dict[eva] + ')')
+        # xticks([-np.pi, -np.pi / 2, 0, np.pi / 2, np.pi],
+        #        [r'$-pi$', r'$-pi/2$', r'$0$', r'$+pi/2$', r'$+pi$'])
+        nl = 0
+        fw.write(eva + ':' + '\n')
+        for method in eva_dict[eva]:
+            plt.plot(miss_list, eva_dict[eva][method], color=CR[nl], marker=MK[nl], label='$' + method + '$')
+            fw.write(','.join(list(map(str, eva_dict[eva][method]))) + '\n')
+            nl += 1
+
+        plt.legend(loc='best')
+        plt.savefig(img_dir + 'compare_PI_' + '_' + eva + '.png')
+        plt.close()
+    fw.close()
+    return 0
+
+def ori_imputation(miss_data,W,ori_speeddata,ori_W):
+    ds = miss_data.shape
+    rW = W|(ori_W==False)
+    est_Km = Kmeans_ha(miss_data, W, 4, 100, 1e-4, 0.7)
+    plt.xlim(0,50)
+    plt.ylim(0,50)
+    plt.plot([0,50],[0,50],color='b',linewidth=2)
+    #plt.scatter(1, 2, s=10, c='r')
+    r = 1
+    for j in range(ds[1]):
+            for k in range(ds[2]):
+                if not rW[r,j,k]:
+                    plt.scatter(ori_speeddata[r,j,k],est_Km[r,j,k],s=10,c='r')
+    plt.savefig('填充真实对比图.png')
+    plt.close()
+    return
+
+if __name__ == '__main__':
+    data_dir = './data/'
+    img_dir = './img_test/'
+    mat_path = '/home/qiushye/2013_east/2013_east_speed.mat'
+    #数据：日期数，线圈数量，时间间隔数
+    #data_size = (60,80,144) 
+    #data_size = (15,35,288)
+    data_size = (30,20,72)
+    #广州数据
+
+    ori_speeddata = scio.loadmat('../GZ_data/60days_tensor.mat')['tensor']
+    #train_sim(ori_speeddata[:,:30,:])
+    #assign_group(ori_speeddata[:,:30,:])
+    #simMat = multi_sim(ori_speeddata[:,:30,:])
+    #scio.savemat('road_sim.mat',{'sim':simMat})
+    #sys.exit()
+    #print(np.var(ori_speeddata))
+    shorten = False
+    print((ori_speeddata==0).sum()/ori_speeddata.size)
+    ori_speeddata,ori_W = deal_orimiss(ori_speeddata,shorten)
+    data_size = np.shape(ori_speeddata)
+    print(data_size)
+
+    # compare_methods(ori_speeddata,ori_W)
+    # compare_PI(ori_speeddata,ori_W)
+    #sys.exit()
+    miss_ratio = 0.2
+    miss_path = data_dir+'miss_'+str(round(miss_ratio,1))+'_'+'_'.join([str(ch) for ch in data_size])+'.mat'
+    #miss_path = data_dir+'cont_miss_'+'_'.join([str(ch) for ch in data_size])+'.mat'
+    if not os.path.exists(miss_path):
+        gene_rand_sparse(ori_speeddata,miss_ratio,miss_path)
+    miss_data,W_miss,tm_ratio = get_sparsedata(miss_path)
+    # f, ax = plt.subplots()
+    # sns.heatmap(miss_data[0], cmap='RdBu', linewidths=0.05, ax=ax)
+    # ax.invert_yaxis()
+    # ax.set_xlabel('time interval')
+    # ax.set_ylabel('days')
+    # plt.savefig(img_dir+'miss_heatmap.png')
+    # plt.close()
+    # sys.exit()
+    W = miss_data>0
+    #W1 = miss_data==0
+    data_shape = np.shape(ori_speeddata)
+    miss_data = pre_impute(miss_data,W)
+    print('pre_impute:', rmse_mape_rse(miss_data, ori_speeddata, W | (ori_W == False)))
+    #compare_3d_4d(ori_speeddata,miss_data)
+    ori_imputation(miss_data, W, ori_speeddata, ori_W)
+    #svd_vary(miss_data)
+
+    sys.exit()
+    #est_partmiss(ori_speeddata, ori_W, miss_data, W, 0.04)
+    #u,sigma,vt = np.linalg.svd(dtensor(miss_data).unfold(1),0)
+    #print(sigma[0],sigma[0]/sum(sigma))
+    #sys.exit()
+    lou = 1e-3
+    K = 100
+    conv = 1e-4
+
+
+   
